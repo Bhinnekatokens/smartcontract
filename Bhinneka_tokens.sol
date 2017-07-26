d17fd56f7ef6dd5e3c4ad9cade9a5a62f742cde0
Iqbl26 <iqball26@hotmail.com>
2017-07-26 20:34:43 +0700

    Update Bhinneka_tokens.sol

--- Bhinneka_tokens.sol
+++ Bhinneka_tokens.sol
@@ -1,53 +1,69 @@
-pragma solidity ^0.4.6;
- 
-contract SafeMath {
-  //internals
- 
-  function safeMul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
- 
-  function safeSub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
- 
-  function safeAdd(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
- 
-}
- 
-contract BNKToken is SafeMath {
+pragma solidity ^0.4.8;
+contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
+
+contract MyToken {
     /* Public variables of the token */
     string public standard = 'ERC20';
-    string public name = 'BHINNEKA TOKEN';
-    string public symbol = 'BNK';
-    uint8 public decimals = 8;
-    uint256 public totalSupply = 26000000;
-    address public owner = 0x7D98fa238C3bBF0f2BCD1bcE19eFA028474B4E07;
- 
-    /* This creates an array with all the balances */
+    string public name =  'Bhinneka Tokens';
+    string public symbol = 'BNK' ;
+    uint8 public decimals = '8';
+    uint256 public totalSupply '2600000000000000';
+
+    /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
- 
- 
-    /* This generates a public event on the blockchain that will notify all clients */
+
+    /* This generates a public event on the blockchain that will notify clients */
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Burned(uint amount);
- 
- 
-    /* A contract or  person attempts to get the tokens of somebody else. */
+
+    /* This notifies clients about the amount burnt */
+    event Burn(address indexed from, uint256 value);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function MyToken(
+        uint256 initialSupply,
+        string tokenName,
+        uint8 decimalUnits,
+        string tokenSymbol
+        ) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value)
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /* Approve and then communicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }        
+
+    /* A contract attempts to get the coins */
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        var _allowance = allowance[_from][msg.sender];
-        balanceOf[_from] = safeSub(balanceOf[_from],_value); // Subtract from the sender
-        balanceOf[_to] = safeAdd(balanceOf[_to],_value);     // Add the same to the recipient
-        allowance[_from][msg.sender] = safeSub(_allowance,_value);
+        balanceOf[_from] -= _value;                           // Subtract from the sender
+        balanceOf[_to] += _value;                             // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
         Transfer(_from, _to, _value);
         return true;
     }
